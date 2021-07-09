5c8b97cfae60538268f800d6ae33cb92ba03d1b8
David Barrat <david@barrat.io>
2021-07-09 03:50:35 +0200

    adding semi colon option to prettier

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,5 +3,6 @@
 // export const ALGORITHMS_MUST_BE_ARR_MSG = 'algorithms must be an array';
 export const JWT_CANNOT_BE_DECODED = "impossible to decode jwt"
 export const JWT_MALFORMED_HEADERS = "malformed jwt headers"
+export const JWT_SUPPORTED_ALGS = ["HS256", "JS256"]
 
 export const REGEX_BEARER_HEADERS = /^Bearer$/i
