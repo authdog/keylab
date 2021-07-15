77086500e84e7625c39616917fd9271cb81cd04e
David Barrat <david@barrat.io>
2021-07-15 09:47:18 +0200

    tests: fix script i18n tests

--- scripts/__tests__/i18n.test.ts
+++ scripts/__tests__/i18n.test.ts
@@ -9,7 +9,7 @@ it("generate correctly frontmatter config", () => {
     const testObj1 = { foo: "bar" }
     expect(generateFrontMatter(testObj1)).toEqual(expectedWithFooFieldObject);
 
-    const expectedWithDocusaurusConfigFm = "---\nhide_title: true\sidebar_label: 'Some text'\n---";
+    const expectedWithDocusaurusConfigFm = "---\nhide_title: true\nsidebar_label: Some text\n---";
     const testObj2 = { hide_title: true, sidebar_label: 'Some text' }
     expect(generateFrontMatter(testObj2)).toEqual(expectedWithDocusaurusConfigFm);
 });
