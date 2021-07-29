39414611aafb32c352577413913ced7840b6992d
David Barrat <david@barrat.io>
2021-07-29 16:31:57 +0200

    removing pass with no test

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,7 @@ module.exports = {
         "^.+\\.(ts)$": "ts-jest",
     },
     testPathIgnorePatterns: [
+        // https://ilikekillnerds.com/2019/10/jest-not-finding-tests-in-travis-ci-you-might-be-ignoring-the-build-folder/
         "/node_modules/", "dist", "docs", "coverage", "tsdoc"
     ],
     testMatch: [
